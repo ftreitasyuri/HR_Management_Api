5fdbcffb94e94b4c578073d2ca47de03be9a655f
ftreitasyuri <yuriftq@gmail.com>
2025-06-18 19:22:18 -0300

    Upload: Subindo config no cors para liberar o acesso do vercel https://hr-management-xi-eight.vercel.app

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://hr-management-xi-eight.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
